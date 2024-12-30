7573f6c74b4dd5ac94179470d1ac1e559f25b350
Safeyra <88963268+Safeyra@users.noreply.github.com>
2024-12-30 23:19:11 +0300

    Update HeartstringsQuiz.js

--- HeartstringsQuiz.js
+++ HeartstringsQuiz.js
@@ -370,7 +370,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     backButton.addEventListener('click', function() {
         // Redirect to the home page or perform any other action you want
-        window.location.href = 'https://gudetea.github.io/FruitCard-Odyssey/'; // Replace 'home.html' with the actual URL of your home page
+        window.location.href = 'https://safeyra.github.io/HeartstringsQuiz/'; // Replace 'home.html' with the actual URL of your home page
     });
 });
 
